41d1b46c0d48cdc45c185a772af90805c3f781ce
Nikita <266900@via.dk>
2020-11-19 13:39:59 +0100

    functional-forklift + tutorials

---
+++ Assets/VRVikar/Forklift-Nikita/DriveCarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveCarController : MonoBehaviour
+{
+    
+    
+    public float driveSpeed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate () {
+      
+        
+        // Go forward
+        if (Input.GetKey(KeyCode.W))
+        {
+                rb.AddForce(transform.forward * driveSpeed* 10);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+                rb.AddForce(Vector3.back * driveSpeed * 10);
+        }
+
+
+    }
+
+
+}
